5048510ed9e19c819c4eba6d00767b69e4a3b0cc
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-09-23 14:14:44 -0500

    set server

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+
+const app = express();
+
+// configure mongoose to connect to mongodb
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+}).then(res => {
+  console.log("MongoDB connected succesfully")
+}).catch(err => {
+  console.log("MongoDB connection failed.")
+});
+
+// configure express
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+app.use('/', (req, res) => {
+  res.send("Welcome to the rice fields!");
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+})
