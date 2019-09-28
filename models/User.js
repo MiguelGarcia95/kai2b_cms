9d9a51a9995f255567bacdeff890dd563edc8d5b
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-09-28 11:18:08 -0500

    add authentication

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new Schema({
   name: {
