98ee74d7dc1e42b43ff078f16dcf71b9c6b12aff
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-10-07 17:53:51 -0500

    break up routes and controller files

--- app.js
+++ app.js
@@ -60,7 +60,6 @@ const adminRoutes = require('./routes/admin/index');
 app.use('/', defaultRoutes);
 app.use('/admin', adminRoutes);
 
-
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 })
--- routes/default/index.js
+++ routes/default/index.js
@@ -1,20 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const posts = require('./posts');
-const comments = require('./comments');
-const users = require('./users');
-const categories = require('./categories');
-
 router.all('/*', (req, res, next) => {
   req.app.locals.layout = 'default';
   next();
 })
 
-router.use('/', posts);
-router.use('/', comments);
-router.use('/', users);
-router.use('/', categories);
+router.use('/', require('./posts'));
+router.use('/', require('./comments'));
+router.use('/', require('./users'));
+router.use('/', require('./categories'));
 
 module.exports = router;  
 
